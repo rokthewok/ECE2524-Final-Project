8b3b60b63861e85d2ca818b227976aebcd93505a
Kevin Malachowski <kevin@chowski.com>
2012-04-28 15:25:13 -0700

    Added animation to the player.

--- player.py
+++ player.py
@@ -6,9 +6,12 @@ from pygame.locals import *
 
 class Player(pygame.sprite.Sprite):
 	
-	def __init__(self, filename):
+	def __init__(self, filename1, filename2):
 		pygame.sprite.Sprite.__init__(self) # initialize the base class
-		self.image = pygame.image.load(filename) # load an image for the character
+		self._image0 = pygame.image.load(filename1) # load an image for the character
+		self._image1 = pygame.image.load(filename2) # load an image for the character
+		self.image = self._image0
+		
 		self.rect = self.image.get_rect() # get the enclosing rectangle for the image
 		self.jumping = False # jumping flag
 		self.can_jump = True
@@ -16,7 +19,20 @@ class Player(pygame.sprite.Sprite):
 		
 		self.rect = self.rect.move( 200, 200 ) # set start location
 
+		self.cur_image = 0
+		self.animation_count = 0
+
 	def update(self):
+		self.animation_count -= 1
+		if self.animation_count <= 0:
+			self.animation_count = 20
+			self.cur_image = (self.cur_image + 1) % 2
+
+			if self.cur_image == 0:
+				self.image = self._image0
+			else:
+				self.image = self._image1
+		
 		if self.jumping:
 			# if the jumping flag is true,
 			# call the jump function
