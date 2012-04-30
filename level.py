d1af120d147bd2881f54cf776bdfab238943d3a8
John Ruffer <jqruffer@vt.edu>
2012-04-30 00:02:15 -0400

    reconfigured level class; background now wraps.

---
+++ level.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import *
+
+class Level(object):
+
+	def __init__(self,filename,music_file):
+		self._image0 = pygame.image.load(filename)
+		self._image1 = pygame.image.load(filename)
+		self._rect0 = self._image0.get_rect(topleft=(0,0))
+		self._rect1 = self._image1.get_rect(topleft=(2048,0))
+		self.move = -2
+		
+		# once we have music, this will play it for the level
+		#self.music = pygame.mixer.music.load(music_file)
+		#self.music.pygame.mixer.music.play(-1) # loop infinitely
+
+	def update(self):
+		# checks to see if the background has scrolled off the screen; if it has, move it back to the start
+		if self._rect0.right == 0:
+			self._rect0.left = 2048
+		if self._rect1.right == 0:
+			self._rect1.left = 2048
+
+		self._rect0 = self._rect0.move( self.move, 0 )
+		self._rect1 = self._rect1.move( self.move, 0 )
+
+	def draw(self,screen):
+		screen.blit( self._image0,self._rect0 )
+		screen.blit( self._image1, self._rect1 )
