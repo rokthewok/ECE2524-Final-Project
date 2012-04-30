f62586724c0002ca279e351a777f857e790934f3
John Ruffer <jqruffer@vt.edu>
2012-04-30 00:17:14 -0400

    things were not working. fixed.

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ import menu
 
 pygame.init()
 # initialize objects here
-<<<<<<< HEAD
-player = player.Player("dog_cape1.png", "dog_cape2.png")
-level = level.Level("background.png","level_one_music.mp3")
-test = Entity.Entity(("dog_cape1.png","dog_cape2.png"),[0,200],3)
-=======
+#<<<<<<< HEAD
+#player = player.Player("dog_cape1.png", "dog_cape2.png")
+level_one = level.Level("Images//background.png","level_one_music.mp3")
+test = Entity.Entity(("Images//dog_cape1.png","Images//dog_cape2.png"),[0,200],3)
+#=======
 player = player.Player(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
 level = level.Level("Images//Background.png","level_one_music.mp3")
->>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
+#>>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
 
 window = pygame.display.set_mode((1024, 384))
 pygame.display.set_caption('Go Dog, Go!')
@@ -25,11 +25,11 @@ screen = pygame.display.get_surface()
 
 clock = pygame.time.Clock()
 	
-<<<<<<< HEAD
+#<<<<<<< HEAD
 objects = pygame.sprite.OrderedUpdates((player,test))
-=======
-objects = pygame.sprite.OrderedUpdates((level,player))
->>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
+#=======
+#objects = pygame.sprite.OrderedUpdates((level,player))
+#>>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
 
 # open the starting menu
 main_menu = menu.Menu( "Images//main_menu.png" )
@@ -55,12 +55,12 @@ while True:
 
 	screen.fill((0,0,0))
 	# update and draw objects
-<<<<<<< HEAD
+#<<<<<<< HEAD
 	#test.update(time_passed_seconds)
-	level.update()
-	level.draw(screen)
-=======
->>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
+	level_one.update()
+	level_one.draw(screen)
+#=======
+#>>>>>>> 6549b0665fb113cdfbf65cd8d65b04b224929758
 	objects.update(elapsed_time)
 	objects.draw(screen)
 	pygame.display.flip()
