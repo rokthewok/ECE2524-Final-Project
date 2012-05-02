6114d68c1caab4168e52909ee72136b5031c6db7
John Ruffer <jqruffer@vt.edu>
2012-05-02 12:06:43 -0400

    Added invincibility for post damage protection; scaled down the player hitbox.

--- player.py
+++ player.py
@@ -6,18 +6,20 @@ from Entity import Entity
 class Player(Entity):
 	
 	def __init__(self, image_paths,fps=10):
-		Entity.__init__(self,image_paths,(200,230),fps,1)
-		#health = health.Health("Images//bone.png")	
+		Entity.__init__(self,image_paths,(200,230),fps,1)	
 		self.jumping = False # jumping flag
 		self.can_jump = True
-		self.health = 3
+		self.health = 10
+		self._invincible = False # for when player collides with an enemy
+		self._invincible_time = 0 # timer for invincibility
 
 		# scale images for player
 		image_set = []
 		for image in self._images:
 			image_set.append( ( pygame.transform.scale( image, (130,67) ) ) )
 		self._images = image_set
-
+		self._rect = self._images[0].get_rect()
+		self._rect.inflate_ip( -self._rect.width / 2, -self._rect.height / 2 )
 	def update(self,t=0):
 		Entity.update(self,t)
 		#print t, self.rect.topleft, self._speed 
@@ -32,15 +34,25 @@ class Player(Entity):
 			if self.rect.top >= 230:
 				self._speed[1] = 0.0
 
+		# limit duration of post damage invincibility (1000 ms)
+		if self._invincible == True:
+			if self._invincible_time < 1000:
+				self._invincible_time += t
+			else:
+				self._invincible = False
+				self._invincible_time = 0
 	def jump(self):
 		"""function which makes the player 'jump'"""	
 		if self.rect.top < 2:
 			self.can_jump = False
 		self.jumping = self.can_jump
 
-	def decrementHealth(self):
-		"""decrement the player's health"""
-		self.health -= 1
+	def takeDamage(self):
+		"""Causes player to take damage"""
+		if self._invincible == False:
+			self.health -= 1
+			# make player invincible for 0.5 seconds
+			self._invincible = True
 
 	def getHealth(self):
 		"""return the player's health"""
