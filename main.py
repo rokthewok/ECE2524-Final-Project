65dfa789ee8da33e996ac5688c2f16fdcaae0ddf
Joseph Sebacher <jdsebach@gmail.com>
2012-05-01 23:15:58 -0400

    fixed some bugs

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ player = player.Player(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
 main_menu = menu.Menu( "Images//main_menu.png" )
 pause_menu = menu.PauseMenu( "Images//pausemenu.png" )
 
-enemy1 = enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
-rock = enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
-bone = enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
-
 window = pygame.display.set_mode((1024, 384))
 pygame.display.set_caption('Go Dog, Go!')
 screen = pygame.display.get_surface()
@@ -34,13 +30,15 @@ clock = pygame.time.Clock()
 
 enemies = []
 for i in range(0,NUM_ENEMIES):
-	enemies.append(enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3))
-	enemies[i].setEnemy(1000*random()+200,384*random(),-5*random()-1)
+	enemies.append(enemy.Enemy(("Images//birdfly.png", "Images//birdglide.png"),3))
+	enemies[i].setEnemy(300*random(),1024,-5*random()-1)
 
-allsprites = pygame.sprite.OrderedUpdates((player,test,enemies,enemy1,rock,bone))
+allsprites = pygame.sprite.OrderedUpdates((player,test,enemies))
 
 # open the starting menu
 main_menu.menu(screen)
+
+score = 0
 
 while True:
 	# catch event
@@ -48,17 +46,6 @@ while True:
 	# update objects
 	elapsed_time = clock.tick(60)
 	#print elapsed_time
-
-	spawnEnemy = random()
-
-	if spawnEnemy > 0.95:
-		pickEnemy = 3*random()
-		if pickEnemy < 1 & (not enemy1.moving):
-			enemy1.setEnemy(200, 500, -5)
-		elif pickEnemy < 2 & (not rock.moving):
-			rock.setEnemy(100,500, -5)
-		elif pickEnemy < 3 & (not bone.moving):
-			bone.setEnemy(300,500, -5)
 			
 	event = pygame.event.poll()
 	#print event	# for debugging purposes
@@ -84,6 +71,8 @@ while True:
 	allsprites.update(elapsed_time)
 	allsprites.draw(screen)
 	
+	score += 1
+
 	pygame.display.flip()
 exit()
 
