dd8c2761787a24b3f9dff1c35527f8604f61d2c6
Ben Smith <bls220@vt.edu>
2012-05-01 21:59:16 -0400

    Added all enemies to allsprites and generated all enemies

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ import menu
 from random import random
 # this contains the main loop of the game
 
+NUM_ENEMIES = 5
+
 pygame.init()
 # initialize objects here
 #player = player.Player("dog_cape1.png", "dog_cape2.png")
 level_one = level.Level("Images//background.png","Images//midground.png","level_one_music.mp3")
 test = Entity.Entity(("Images//dog_cape1.png","Images//dog_cape2.png","Images//background.png","Images//midground.png"),[0,200],3,1)
 player = player.Player(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
-enemy = enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
+
+main_menu = menu.Menu( "Images//main_menu.png" )
 
 window = pygame.display.set_mode((1024, 384))
 pygame.display.set_caption('Go Dog, Go!')
@@ -24,10 +27,15 @@ screen = pygame.display.get_surface()
 
 clock = pygame.time.Clock()
 
-objects = pygame.sprite.OrderedUpdates((player,test,enemy))
+enemies = []
+for i in range(0,NUM_ENEMIES):
+	enemies.append(enemy.Enemy(("Images//dog_cape1.png", "Images//dog_cape2.png"),3))
+	enemies[i].setEnemy(1000*random()+200,384*random(),-5*random()-1)
+
+allsprites = pygame.sprite.OrderedUpdates((player,test,enemies))
+
 
 # open the starting menu
-main_menu = menu.Menu( "Images//main_menu.png" )
 main_menu.menu(screen)
 
 while True:
@@ -36,14 +44,6 @@ while True:
 	# update objects
 	elapsed_time = clock.tick(60)
 	#print elapsed_time
-
-	spawnEnemy = random()
-
-	if spawnEnemy > 0.99:
-		pickEnemy = 1*random()
-		if pickEnemy < 1:
-			enemy.setEnemy(50, 50, 3)
-			
 
 	event = pygame.event.poll()
 	#print event	# for debugging purposes
@@ -66,8 +66,9 @@ while True:
 	level_one.update()
 	level_one.draw(screen)
 
-	objects.update(elapsed_time)
-	objects.draw(screen)
+	allsprites.update(elapsed_time)
+	allsprites.draw(screen)
+	
 	pygame.display.flip()
 exit()
 
