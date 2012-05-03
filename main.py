dc1a3be12e156258befa92e0d82c8f50ab5329ec
John Ruffer <jqruffer@vt.edu>
2012-05-03 19:05:00 -0400

    rearranged game loop to use game states. Useful for scaling up for more levels, more menus, etc.

--- main.py
+++ main.py
@@ -2,24 +2,22 @@
 
 import pygame, sys
 from pygame.locals import *
-import player
-import enemy
-import level
-import Entity
-import menu
-import statcounter
+import player, enemy, level, Entity, menu, statcounter
 from random import random
+from gamestate import GameState
 # this contains the main loop of the game
 
 NUM_ENEMIES = 5
 
 pygame.init()
+#==================================================================================================
 # initialize objects here
 level_one = level.Level("Images//background.png","Images//midground.png","Audio//level_one_music.wav")
 player = player.Player(("Images//dog_cape1.png", "Images//dog_cape2.png"),3)
 
 main_menu = menu.Menu( "Images//main_menu.png", "Audio//main_theme.wav" )
 pause_menu = menu.PauseMenu( "Images//pausemenu.png" )
+gameover_menu = menu.GameOverMenu( "Images//gameovermenu.png" )
 
 window = pygame.display.set_mode((1024, 384))
 pygame.display.set_caption('Go Dog, Go!')
@@ -27,60 +25,79 @@ screen = pygame.display.get_surface()
 
 clock = pygame.time.Clock()
 
+stats = statcounter.StatCounter()
+statsgroup = pygame.sprite.GroupSingle(stats)
+
+state = GameState.MAINMENU
+#==================================================================================================
+
+# create a list of enemies; will change in future updates
 enemies = []
 for i in range(0,NUM_ENEMIES):
 	enemies.append(enemy.Enemy(("Images//birdfly.png", "Images//birdglide.png"),3))
 
+# create a group to update the sprites in order
 allsprites = pygame.sprite.OrderedUpdates((player,enemies))
 
-# open the starting menu
-main_menu.menu(screen)
-# once the main menu is exited, start the level music THIS SHOULD BE ALTERED so we can load any level's music at a given time
-level_one.start_music()
-#create stat counter
-stats = statcounter.StatCounter()
-statsgroup = pygame.sprite.GroupSingle(stats)
-
 while True:
-	# catch event
-	# respond to event
-	# update objects
-	elapsed_time = clock.tick(60)
+
+	if state == GameState.MAINMENU:
+		# open the starting menu
+		main_menu.menu(screen)
+		state = GameState.PLAYING
+
+	if state == GameState.PLAYING:	
+		# once the main menu is exited, start the level music 
+		# NOTE: THIS SHOULD BE ALTERED so we can load any level's music at a given time
+		level_one.start_music()
+
+		while state == GameState.PLAYING:
+			# catch event
+			# respond to event
+			# update objects
+			elapsed_time = clock.tick(60)
 	
-	for guy in enemies:
-		if pygame.sprite.collide_rect( player, guy ):
-			player.takeDamage()
-			#print "Health: %d" % player.getHealth() # test purposes
-		if player.getHealth() == 0:
-			# gameover.gameOver()
-			pass
+			for guy in enemies:
+				if pygame.sprite.collide_rect( player, guy ):
+					player.takeDamage()
+					#print "Health: %d" % player.getHealth() # test purposes
+				if player.getHealth() == 0:
+					state = GameState.GAMEOVER
 
-	event = pygame.event.poll()
-	#print event	# for debugging purposes
-	if event.type == QUIT:
-		sys.exit(0)
+			event = pygame.event.poll()
+			#print event	# for debugging purposes
+			if event.type == QUIT:
+				sys.exit(0)
 
-	if event.type == KEYDOWN:
-		if event.key == K_UP:
-			player.jump()
-		if event.key == K_ESCAPE:
-			pause_menu.menu(screen,clock)
-		#if event.key == K_b:
-			#test.setAnim(0,test._endFrame%4+1)
-			#print "test._startFrame=",test._startFrame," test._endFrame=",test._endFrame
+			if event.type == KEYDOWN:
+				if event.key == K_UP:
+					player.jump()
+				if event.key == K_ESCAPE:
+					pause_menu.menu(screen,clock)
+				#if event.key == K_b:
+					#test.setAnim(0,test._endFrame%4+1)
+					#print "test._startFrame=",test._startFrame," test._endFrame=",test._endFrame
 
-	# update and draw objects
+			# update and draw objects
 
-	#test.update(time_passed_seconds)
-	level_one.update()
-	level_one.draw(screen)
+			level_one.update()
+			level_one.draw(screen)
 	
-	stats.update(player.getHealth(), elapsed_time)
+			stats.update(player.getHealth(), elapsed_time)
 
-	allsprites.update(elapsed_time)
-	allsprites.draw(screen)
-	statsgroup.draw(screen)
+			allsprites.update(elapsed_time)
+			allsprites.draw(screen)
+			statsgroup.draw(screen)
 
-	pygame.display.flip()
+			pygame.display.flip()
+
+	if state == GameState.GAMEOVER:
+		decision = gameover_menu.menu(screen)
+		if decision == True:
+			player.resetHealth()
+			state = GameState.MAINMENU
+		else:
+			sys.exit(0)
+
 exit()
 
