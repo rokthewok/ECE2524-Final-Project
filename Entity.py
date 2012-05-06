5a35ab1f3fa9cc4e459d1435364fede96b8d523f
Kevin Malachowski <kevin@chowski.com>
2012-05-06 12:14:11 -0700

    Added a decay to x/y velocity. Currently, by default, there is no decay (the decay factor is 1.0). This can be set in subclasses and is used by the player class for the new horizontal movement.

--- Entity.py
+++ Entity.py
@@ -12,6 +12,7 @@ class Entity(pygame.sprite.Sprite):
 		self._images = []
 
 		self._speed = [0.0,0.0]
+		self._speedDecay = [1.0, 1.0]
 
 		#Load images and store in _images
 		for image in image_paths:
@@ -34,6 +35,8 @@ class Entity(pygame.sprite.Sprite):
 			self.image = self._images[self._frame]
 			self._last_update = 0
 		#move the entity
+		self._speed[0] *= self._speedDecay[0]
+		self._speed[1] *= self._speedDecay[1]
 		dx = float((t/100.0)*self._speed[0])
 		dy = float((t/100.0)*self._speed[1])
 		self.rect.move_ip(dx,dy)
@@ -42,6 +45,9 @@ class Entity(pygame.sprite.Sprite):
 			self.rect.top = 0
 		if self.rect.top > 230:
 			self.rect.top = 230
+	
+	def moveHoriz(self, dx):
+		self._speed[0] += dx
 	
 	def setSpeed(self,x,y):
 		if not x=='x':
