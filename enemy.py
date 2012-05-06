ebd27b3f39a42bc87de7e5c2de8d84dd80c5b5e9
Kevin Malachowski <kevin@chowski.com>
2012-05-06 12:13:16 -0700

    Made enemies go faster as the game goes on (every time they wrap around they can set their speed to a faster speed).

--- enemy.py
+++ enemy.py
@@ -5,13 +5,15 @@ class Enemy(Entity):
 
 	def __init__(self, image_paths, fps=10):
 		Entity.__init__(self,image_paths,(-9001,-9001),fps,1)
+		self._wrap_count = 0
 
 	def update(self,t=0):
 		Entity.update(self,t)
 		if self.rect.right < 0:
+			self._wrap_count += 2
 			self.rect.left = 1024
 			self.rect.top = 300*random()
-			self.setSpeed(-8.0*random()-7.0,0.0)
+			self.setSpeed(-8.0*random()-10.0-self._wrap_count*random(),0.0)
 		print self._speed
 
 	def setEnemy(self, top, left, speed):
